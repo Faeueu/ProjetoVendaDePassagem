762a3eb71e28d300cb08934a3b60a87a39bddef9
Maciano Dantas <maciano.o.dantas@gamil.com>
2024-04-10 00:04:23 -0300

    teste

--- src/main/java/Model/entities/Rota.java
+++ src/main/java/Model/entities/Rota.java
@@ -51,7 +51,7 @@ public class Rota {
                 "rotaID='" + rotaID + '\'' +
                 ", inicio=" + inicio +
                 ", fim=" + fim +
-                ", onibus=" + onibus +
+                ", onibus= "  + onibus +
                 '}';
     }
 }
